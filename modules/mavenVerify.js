c5d1d05113c90bac670c0a8cd035f2d05aa435bc
folkforms <alan.bergin@gmail.com>
2021-12-29 16:59:47 +0000

    Clean up debug output

--- modules/mavenVerify.js
+++ modules/mavenVerify.js
@@ -1,6 +1,7 @@
 const mavenVerify = shell => {
   const cmd = `mvn -Dcli.version=TEST package`;
-  console.debug(`mavenVerify: cmd = ${cmd}`);
+  console.debug(`DEBUG: mavenVerify:`);
+  console.debug(`DEBUG:     cmd = ${cmd}`);
   shell.exec(cmd);
 }
 
