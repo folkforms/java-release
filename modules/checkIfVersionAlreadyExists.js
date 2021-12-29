c7007b6bbdd8d65c81f4f3bb49d373cbc9d4aa5f
folkforms <alan.bergin@gmail.com>
2021-12-29 16:59:41 +0000

    Move checkIfVersionAlreadyExists function into its own file

---
+++ modules/checkIfVersionAlreadyExists.js
@@ -0,0 +1,21 @@
+const checkIfVersionAlreadyExists = (shell, version) => {
+  console.debug(`DEBUG: checkIfVersionAlreadyExists:`);
+  // FIXME This needs to pull groupId and artifactId out of pom file
+  const cmd = `mvn dependency:get -Dartifact=folkforms:example:${version} > /dev/null 2>&1`;
+  let throwError = true;
+  try {
+    console.debug(`DEBUG:     cmd = ${cmd}`);
+    shell.exec(cmd);
+    console.debug(`DEBUG:     dependency:get found an existing version with that version number`);
+    throwError = true;
+  } catch (error) {
+    console.debug(`DEBUG:     dependency:get did not find an existing version with that version number`);
+    throwError = false;
+  }
+
+  if(throwError) {
+    throw new Error(`Version ${options.version} already exists.`);
+  }
+}
+
+module.exports = checkIfVersionAlreadyExists;
--- modules/mavenRelease.js
+++ modules/mavenRelease.js
@@ -1,23 +1,12 @@
+const checkIfVersionAlreadyExists = require("./checkIfVersionAlreadyExists");
+
 const mavenRelease = (phase, shell, options) => {
-  const code = checkIfVersionAlreadyExists(options.version);
-  if(code !== 0) {
-    throw new Error(`Version ${options.version} already exists.`);
-  }
+  checkIfVersionAlreadyExists(shell, options.version);
 
   const cmd = `mvn -Dcli.version=${options.version} ${phase}`;
-  console.debug(`mavenRelease: cmd = ${cmd}`);
+  console.debug(`DEBUG: mavenRelease:`);
+  console.debug(`DEBUG:     cmd = ${cmd}`);
   shell.exec(cmd);
-}
-
-const checkIfVersionAlreadyExists = version => {
-  const cmd = `mvn dependency:get -Dartifact=folkforms:example:${version} -o -DrepoUrl=${process.env.MAVEN_REPO}`;
-  console.debug(`checkIfVersionAlreadyExists: cmd = ${cmd}`);
-  try {
-    shell.exec(cmd);
-    return 1;
-  } catch (error) {
-    return 0;
-  }
 }
 
 module.exports = mavenRelease;
