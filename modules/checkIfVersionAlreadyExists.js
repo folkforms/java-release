e6a445fe5436369feb555044eb11faceabb4b551
folkforms <alan.bergin@gmail.com>
2021-12-29 17:29:24 +0000

    Fix error not always being thrown when checking version

--- modules/checkIfVersionAlreadyExists.js
+++ modules/checkIfVersionAlreadyExists.js
@@ -3,19 +3,19 @@ const checkIfVersionAlreadyExists = (shell, version) => {
   // FIXME This needs to pull groupId and artifactId out of pom file
   const cmd = `mvn dependency:get -Dartifact=folkforms:example:${version} > /dev/null 2>&1`;
   let throwError = true;
-  let r = 1;
+  let code = 1;
   try {
     console.debug(`DEBUG:     cmd = ${cmd}`);
-    r = shell.exec(cmd).code;
+    code = shell.exec(cmd).code;
     console.debug(`DEBUG:     dependency:get found an existing version with that version number`);
     throwError = true;
   } catch (error) {
     console.debug(`DEBUG:     dependency:get did not find an existing version with that version number`);
     throwError = false;
-    r = 0;
+    code = 0;
   }
 
-  if(throwError || r.code !== 0) {
+  if(throwError || code !== 0) {
     throw new Error(`Version ${version} already exists.`);
   }
 }
